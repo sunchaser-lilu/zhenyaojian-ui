e031b8ae0ff544233356e77f1990bbf52aa53e9d
lilu <admin@lilu.org.cn>
2022-11-25 21:16:04 +0800

    feat: add spring boot admin

---
+++ src/pages/observability/sba/SpringBootAdmin.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <i-frame :src="url" />
+  </div>
+</template>
+<script>
+import IFrame from '@/components/IFrame/IFrame'
+export default {
+  name: 'SpringBootAdmin',
+  components: { IFrame },
+  data() {
+    return {
+      url: process.env.VUE_APP_API_BASE_URL + '/observe/server'
+    }
+  },
+  created() {
+    console.log(this.url)
+  }
+}
+</script>
