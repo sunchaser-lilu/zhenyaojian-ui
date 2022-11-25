34b31c7ca7bdbbe2136340c678f8b4d05a7bd75c
lilu <admin@lilu.org.cn>
2022-11-25 21:15:54 +0800

    fix: fix some error link

--- src/config/default/setting.config.js
+++ src/config/default/setting.config.js
@@ -19,7 +19,7 @@ module.exports = {
   cachePage: true, // 是否缓存页面数据，仅多页签模式下生效，true 缓存, false 不缓存
   hideSetting: false, // 隐藏设置抽屉，true:隐藏，false:不隐藏
   systemName: '镇妖剑管理系统', // 系统名称
-  copyright: '2018 ICZER 工作室出品', // copyright
+  copyright: '2022-present SunChaser', // copyright
   asyncRoutes: true, // 异步加载路由，true:开启，false:不开启
   showPageTitle: false, // 是否显示页面标题（PageLayout 布局中的页面标题），true:显示，false:不显示
   filterMenu: true, // 根据权限过滤菜单，true:过滤，false:不过滤
--- src/layouts/footer/PageFooter.vue
+++ src/layouts/footer/PageFooter.vue
@@ -7,7 +7,8 @@
         :href="item.link ? item.link : 'javascript: void(0)'"
         v-for="(item, index) in linkList"
       >
-        <a-icon v-if="item.icon" :type="item.icon" />{{ item.name }}
+        <a-icon v-if="item.icon" :type="item.icon" />
+        {{ item.name }}
       </a>
     </div>
     <div class="copyright">Copyright<a-icon type="copyright" />{{ copyright }}</div>
