184d047276cbd3d664045bd2ed83ca3bd4596016
lilu <admin@lilu.org.cn>
2022-11-25 21:15:19 +0800

    feat: add IFrame component

---
+++ src/components/IFrame/IFrame.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :style="'height:' + height">
+    <iframe :src="src" frameborder="no" style="width: 100%; height: 100%" scrolling="auto" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      height: document.documentElement.clientHeight + 'px',
+      url: this.src
+    }
+  },
+  mounted: function () {
+    const _this = this
+    window.onresize = function temp() {
+      _this.height = document.documentElement.clientHeight + 'px;'
+    }
+  }
+}
+</script>
